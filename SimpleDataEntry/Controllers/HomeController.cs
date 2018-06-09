d316921323650596a44d4b9cd5ff41e26f66dc46
JingaFoo <calvinmwariama@yahoo.com>
2018-06-09 16:42:51 -0400

    First Commit

---
+++ SimpleDataEntry/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleDataEntry.Models;
+
+namespace SimpleDataEntry.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [Route("/rsvpform")]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
